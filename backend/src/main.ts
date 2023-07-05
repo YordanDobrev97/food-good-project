d6118045722f1f28f03c15412136c6e3f0e0b36b
Yordan Dobrev <yordan.dobrev@amusnet.com>
2023-07-06 00:36:42 +0300

    setup backend with express and typescript

---
+++ backend/src/main.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response , Application } from 'express'
+
+const app: Application = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to food & good backend rest api!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
